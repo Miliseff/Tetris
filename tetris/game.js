9bc51a2176d086eb314a5b5107a826001c515635
Milagros Seffino <88240589+Miliseff@users.noreply.github.com>
2021-09-06 09:26:41 -0300

    Add files via upload

---
+++ tetris/game.js
@@ -0,0 +1,237 @@
+et canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+const HEIGHT = 500;
+const WIDTH = 300;
+
+const PIECES = [
+
+];
+
+class Score
+{
+    constructor()
+    {
+        this.score = 0;
+    }
+
+    draw()
+    {
+        ctx.textAlign = "center";
+        ctx.font ='100px Arial, sans-serif';
+        ctx.fillStyle="#A3A3A3";
+        ctx.fillText(""+this.score,WIDTH/2,120);
+        ctx.font ='15px Arial, sans-serif';
+        ctx.fillText("Rolando Andrade",WIDTH/2,30);
+    }
+
+    add()
+    {
+        this.score+=150;
+    }
+}
+
+class Game
+{
+    constructor()
+    {
+        this.gameOver = true;
+
+        this.background = new Rectangle(0,0,WIDTH,HEIGHT, "#424242");
+        this.fabric = new FabricOfPieces();
+
+        let a = this;
+        document.addEventListener("keydown",function (e)
+        {
+            switch (e.keyCode)
+            {
+                case 38:
+                    a.piece.rotateLeft(a.drop);
+                    a.draw();
+                    break;
+                case 37:
+                    a.piece.moveLeft(a.drop);
+                    a.draw();
+                    break;
+                case 39:
+                    a.piece.moveRight(a.drop);
+                    a.draw();
+                    break;
+                case 40:
+                    a.piece.rotateRight(a.drop);
+                    a.draw();
+                    break;
+                case 13:
+                    a.init();
+                default:
+                    a.loop();
+            }
+        });
+    }
+
+
+    init()
+    {
+        this.gameOver = false;
+        this.score = new Score();
+        this.generateRandom();
+        this.drop = [];
+        this.loop();
+    }
+
+    generateRandom()
+    {
+        this.piece = this.fabric.generateRandom();
+        this.piece.setRotation(Math.floor(Math.random()*5));
+    }
+
+    draw()
+    {
+        this.background.draw();
+        this.score.draw();
+        this.drop.forEach((e) => e.draw());
+        this.piece.draw();
+    }
+
+    deleteEmpty()
+    {
+        let i = 0;
+        while(i<this.drop.length)
+        {
+            if(this.drop[i].blocks.length===0)
+            {
+                this.drop.splice(i,1);
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+
+    deleteRow(rowY, yT)
+    {
+        this.runDrop((y, e, i)=>
+        {
+            if(y === rowY)
+            {
+                e.break(yT+rowY*BLOCK_SIZE);
+                return true;
+            }
+            return false;
+        });
+        this.drop.forEach((e)=>e.moveBroke(yT+rowY*BLOCK_SIZE));
+        this.score.add();
+        this.deleteEmpty();
+    }
+
+    runDrop(fun)
+    {
+        let p = this.piece;
+        let yT = p.yT;
+        let yB = p.yB;
+        let length = (yB-yT)/BLOCK_SIZE;
+
+        let Break;
+        this.drop.forEach((e, i) =>
+        {
+            if (e.yT < yB && e.yB > yT)
+            {
+                try
+                {
+                    e.blocks.forEach((a) =>
+                    {
+                        let y = (a.y - yT) / BLOCK_SIZE;
+                        if (y > -1 && y < length)
+                        {
+                            if(fun(y, e, i))
+                            {
+                                throw Break;
+                            }
+
+                        }
+                    })
+                }
+                catch (e)
+                {
+
+                }
+
+            }
+        });
+    }
+
+    rowFilled()
+    {
+
+        let p = this.piece;
+        let length = (p.yB-p.yT)/BLOCK_SIZE;
+        let arr = new Array(length).fill(0);
+        this.runDrop((y)=>{arr[y]++; return false});
+        for(let i = length -1 ; i >= 0; i--)
+        {
+            if(arr[i] === WIDTH/BLOCK_SIZE)
+            {
+                this.deleteRow(i, p.yT);
+            }
+        }
+    }
+
+    collision()
+    {
+        if(this.piece.collision(this.drop))
+        {
+            this.drop.unshift(this.piece);
+            this.rowFilled();
+            this.checkGameOver();
+            this.generateRandom();
+        }
+    }
+
+    checkGameOver()
+    {
+        if(this.piece.y===0)
+        {
+            this.gameOver = true;
+        }
+    }
+
+    loop()
+    {
+        if(!this.gameOver)
+        {
+            this.collision();
+            this.piece.moveY(this.drop);
+            this.draw();
+        }
+        else
+        {
+            this.background.draw();
+            try
+            {
+                this.draw();
+            }
+            catch (e)
+            {
+
+            }
+
+            ctx.textAlign = "center";
+            ctx.font ='30px Arial, sans-serif';
+            ctx.fillStyle="#f7ffb8";
+            ctx.fillText("GAME OVER", WIDTH/2,250);
+            ctx.font ='15px Arial, sans-serif';
+            ctx.fillText("Presiona ENTER para continuar", WIDTH/2,270);
+        }
+
+
+    }
+}
+
+let game = new Game();
+
+function loop()
+{
+    game.loop();
+}
+window.setInterval(loop,200);
---
+++ tetris/pieces.js
@@ -0,0 +1,322 @@
+const BLOCK_SIZE = 20;
+const COLORS = ["#fff47e", "#ff6d4a",
+                "#ff3d49","#46dbff",
+                "#77ff52","#ff519c"];
+
+class Piece
+{
+    constructor(x, y)
+    {
+        this.blocks = [];
+        this.shapes = [];
+        this.rotation = 0;
+        this.rotationMod = 1;
+        this.x = x;
+        this.y = y;
+        this.color = COLORS[Math.floor(Math.random() * COLORS.length)];
+        this.xR = 0;
+        this.yB = 0;
+        this.xL = 0;
+        this.yT = 0;
+        this.broke = false;
+    }
+
+    draw()
+    {
+        this.blocks.forEach((e) => e.draw());
+    }
+
+    generateFromMatrix(matrix)
+    {
+        this.blocks = [];
+        this.xL = 1000;
+        this.yT = 1000;
+        this.xR = 0;
+        this.yB = 0;
+        matrix.forEach((e, i) =>
+        {
+            e.forEach((e, j) =>
+            {
+                if (e === 1)
+                {
+                    let newX = this.x + j * BLOCK_SIZE;
+                    let newY = this.y + i * BLOCK_SIZE;
+                    this.blocks.push(new Rectangle(newX, newY, BLOCK_SIZE, BLOCK_SIZE, this.color));
+                    this.xL = Math.min(this.xL, newX);
+                    this.yT = Math.min(this.yT, newY);
+                    this.xR = Math.max(this.xR, newX + BLOCK_SIZE);
+                    this.yB = Math.max(this.yB, newY + BLOCK_SIZE);
+                }
+            })
+        });
+    }
+
+    collisionWhenRotate(blocks, fun)
+    {
+        if (this.xL < 0 || this.xR > WIDTH || this.collideWith(blocks, (i,j,k)=>this.collideSideWith(i,j,k)))
+        {
+            fun();
+        }
+    }
+
+    collideWith(blocks, fun)
+    {
+        for (let k = 0; k < blocks.length; k++)
+        {
+            let piece = blocks[k];
+            for (let i = 0; i < this.blocks.length; i++)
+            {
+                for (let j = 0; j < piece.blocks.length; j++)
+                {
+                    if (fun(i, j, piece))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    rotateRight(pieces)
+    {
+        this.rotation = (this.rotation + 1) % this.rotationMod;
+        this.generateFromMatrix(this.shapes[this.rotation]);
+        this.collisionWhenRotate(pieces, () => this.rotateRight(pieces));
+    }
+
+    rotateLeft(pieces)
+    {
+        if (this.rotation === 0)
+        {
+            this.rotation = this.shapes.length - 1;
+        } else
+        {
+            this.rotation--;
+        }
+        this.generateFromMatrix(this.shapes[this.rotation]);
+        this.collisionWhenRotate(pieces, () => this.rotateLeft(pieces));
+    }
+
+    setRotation(rotation)
+    {
+        this.rotation = rotation % this.rotationMod;
+        this.generateFromMatrix(this.shapes[this.rotation]);
+    }
+
+    moveY(pieces)
+    {
+        if(!this.collision(pieces))
+        {
+            this.blocks.forEach((e) => (e.y += BLOCK_SIZE));
+            this.y += BLOCK_SIZE;
+            this.yB += BLOCK_SIZE;
+            this.yT += BLOCK_SIZE;
+            return false;
+        }
+        return true;
+    }
+
+    moveX(delta)
+    {
+        this.blocks.forEach((e) => (e.x += delta));
+        this.x += delta;
+        this.xR += delta;
+        this.xL += delta;
+    }
+
+    moveLeft(pieces)
+    {
+        if (this.xL > 0)
+        {
+            this.moveX(-BLOCK_SIZE);
+            if(this.collideWith(pieces, (i,j,piece)=>this.collideSideWith(i,j,piece)))
+            {
+                this.moveX(BLOCK_SIZE);
+            }
+        }
+    }
+
+    moveRight(pieces)
+    {
+        if (this.xR < WIDTH)
+        {
+            this.moveX(BLOCK_SIZE);
+            if(this.collideWith(pieces, (i,j,piece)=>this.collideSideWith(i,j,piece)))
+            {
+                this.moveX(-BLOCK_SIZE);
+            }
+        }
+    }
+
+    collideUpWith(i, j, piece)
+    {
+        return this.blocks[i].x === piece.blocks[j].x && piece.blocks[j].y === this.blocks[i].y + BLOCK_SIZE;
+    }
+
+    collideSideWith(i, j, piece)
+    {
+        return this.blocks[i].x === piece.blocks[j].x && piece.blocks[j].y === this.blocks[i].y;
+    }
+
+    collision(blocks)
+    {
+        if (this.yB >= HEIGHT)
+        {
+            return true;
+        }
+
+        return this.collideWith(blocks, (i, j, piece) => this.collideUpWith(i, j, piece));
+    }
+
+    break(y)
+    {
+        let len = this.blocks.length;
+        this.blocks = this.blocks.filter((a)=>a.y!==y);
+    }
+
+    moveBroke(y)
+    {
+        this.xL = 1000;
+        this.yT = 1000;
+        this.xR = 0;
+        this.yB = 0;
+        this.blocks.forEach((a)=>
+        {
+            if(a.y < y)
+            {
+                a.y += BLOCK_SIZE;
+            }
+            this.xL = Math.min(this.xL, a.x);
+            this.yT = Math.min(this.yT, a.y);
+            this.xR = Math.max(this.xR, a.x + BLOCK_SIZE);
+            this.yB = Math.max(this.yB, a.y + BLOCK_SIZE);
+        });
+        this.x = this.xL;
+        this.y = this.yT;
+    }
+}
+
+class PieceZ1 extends Piece
+{
+    constructor(x, y)
+    {
+        super(x, y);
+        this.shapes = [
+
+            [[1,1,0],
+             [0,1,1]],
+
+            [[0,1],
+             [1,1],
+             [1,0]]
+
+            ];
+        this.rotationMod = 2;
+        this.generateFromMatrix(this.shapes[0]);
+    }
+}
+
+class PieceZ2 extends Piece
+{
+    constructor(x, y)
+    {
+        super(x, y);
+        this.shapes = [
+
+            [[0,1,1],
+             [1,1,0]],
+
+            [[1,0],
+             [1,1],
+             [0,1]]
+
+        ];
+        this.rotationMod = 2;
+        this.generateFromMatrix(this.shapes[0]);
+    }
+}
+
+class PieceI extends Piece
+{
+    constructor(x, y)
+    {
+        super(x, y);
+        this.shapes = [
+
+            [[1,1,1,1]],
+
+            [[0,1],
+             [0,1],
+             [0,1],
+             [0,1]]
+
+        ];
+        this.rotationMod = 2;
+        this.generateFromMatrix(this.shapes[0]);
+    }
+}
+
+class PieceH extends Piece
+{
+    constructor(x, y)
+    {
+        super(x, y);
+        this.shapes = [
+
+            [[1,1],
+             [1,1]],
+
+        ];
+        this.rotationMod = 1;
+        this.generateFromMatrix(this.shapes[0]);
+    }
+}
+
+class PieceL extends Piece
+{
+    constructor(x, y)
+    {
+        super(x, y);
+        this.shapes = [
+
+            [[1,0,0],
+             [1,1,1]],
+
+            [[0,1,1,0],
+             [0,1,0,0],
+             [0,1,0,0]],
+
+            [[1,1,1],
+             [0,0,1]],
+
+            [[0,0,1,0],
+             [0,0,1,0],
+             [0,1,1,0]]
+
+        ];
+        this.rotationMod = 4;
+        this.generateFromMatrix(this.shapes[0]);
+    }
+}
+
+class FabricOfPieces
+{
+    generateRandom()
+    {
+        const iX = 120;
+        let a = Math.floor(Math.random()*5);
+        switch (a)
+        {
+            case 0:
+                return  new PieceZ1(iX,0);
+            case 1:
+                return new PieceZ2(iX,0);
+            case 2:
+                return new PieceL(iX,0);
+            case 3:
+                return new PieceI(iX,0);
+        }
+        return new PieceH(iX,0);
+    }
+}
